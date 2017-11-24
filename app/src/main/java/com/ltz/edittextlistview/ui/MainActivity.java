8c6e198b7337573d2c4a13be11dce7dfa8b971f6
LTzuo <644148112@qq.com>
2017-11-24 08:32:14 +0800

    解决软键盘弹出底部布局上移的问题

--- app/src/main/java/com/ltz/edittextlistview/ui/MainActivity.java
+++ app/src/main/java/com/ltz/edittextlistview/ui/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -45,6 +46,10 @@ public class MainActivity extends AppCompatActivity {
             setContentView(R.layout.listview_layout);
             initButton1Layout();
         }else if(id == R.id.btn2){
+            /*************软键盘弹出底部布局上移的问题************************/
+            getWindow().setSoftInputMode
+                    (WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN|
+                            WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
             setContentView(R.layout.listview_layout);
             initButton2Layout();
         }else if(id == 1){
